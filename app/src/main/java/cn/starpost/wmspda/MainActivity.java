11ec0ceabbf1164921c1534db719736816a1bdbd
pengdongjian <541874767@qq.com>
2019-08-08 00:26:29 +0800

    edit page

--- app/src/main/java/cn/starpost/wmspda/MainActivity.java
+++ app/src/main/java/cn/starpost/wmspda/MainActivity.java
@@ -1,15 +1,26 @@
 package cn.starpost.wmspda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import cn.starpost.wmspda.activity.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnOk = findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: ");
+                toastShow("click");
+            }
+        });
     }
 }
--- app/src/main/java/cn/starpost/wmspda/activity/BaseActivity.java
+++ app/src/main/java/cn/starpost/wmspda/activity/BaseActivity.java
@@ -1,18 +1,29 @@
 package cn.starpost.wmspda.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
-    private final String TAG = getClass().getName();
+    protected final String TAG = getClass().getName();
+    protected final BaseActivity currentObj = this;
 
-    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onCreate: ");
+        Log.d(TAG, currentObj.getClass().getName());
         super.onCreate(savedInstanceState);
+    }
+
+
+    /**
+     * 提醒通知
+     *
+     * @param msg
+     */
+    public void toastShow(CharSequence msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
